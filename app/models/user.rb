b00572f64577c2804da5f2988baca89df0a0facc
brugobi <brgobbi@hotmail.com>
2021-07-27 12:02:40 -0400

    update the CreateJwtDenylist migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  Include default devise modules. Others available are:
-  :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable,
          :jwt_authenticatable,
          :registerable,
